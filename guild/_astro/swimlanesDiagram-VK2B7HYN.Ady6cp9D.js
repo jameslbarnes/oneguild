a806e4736f94dd288f227d964c8024168b5d4dd7
James <barnes.james@gmail.com>
2026-08-21 11:31:53 -0400

    Guild: interactive timeline

---
+++ guild/_astro/swimlanesDiagram-VK2B7HYN.Ady6cp9D.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./src.QN9XHvqB.js";import"./chunk-DU6HZSFF.BEtRggqc.js";import"./chunk-75Z2AOVW.CbwKT6H9.js";import"./chunk-P2QGCYS3.D3wyQHmE.js";import"./chunk-PWAF6VOD.xFnD_LFZ.js";import"./chunk-GMAD6QVW.CGqZmMI4.js";import"./chunk-TEH6E4GO.B3A_JdM1.js";import"./chunk-OBVCFTLP.DPu9jgF9.js";import"./Base.astro_astro_type_script_index_1_lang.Oszgu4a9.js";import"./chunk-F27PBJKO.B_ChuBiw.js";import"./chunk-XXDRQBXY.BTiQUmv6.js";import"./chunk-POPQ4Y6H.De5yvgO-.js";import{r as t,t as n}from"./chunk-RHFEMEQ7.SXX7QnzF.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
